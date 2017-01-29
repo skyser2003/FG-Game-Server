b885b825b5db3f1eb0330869bb4564551ef81043
Seung Ho Jang <shjang1992@gmail.com>
2017-01-29 21:51:28 +0900

    Relative path bug fix

--- PacketProtobuf/build_script/build_proto.js
+++ PacketProtobuf/build_script/build_proto.js
@@ -39,7 +39,7 @@ for (var inputDir of inputDirs) {
     }
 }
 
-var cppOutArgs = inputDirs.map(inputDir => { return '--proto_path=' + inputDir }).concat([
+var cppOutArgs = inputDirs.map(inputDir => { return '--proto_path=' + path.resolve(inputDir); }).concat([
     '--cpp_out=' + outputDir,
     '--descriptor_set_out=' + path.resolve(outputDir, 'proto.protobin')
 ]).concat(subDirs);
